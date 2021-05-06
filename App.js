ddb0b6570f556c94ee2b774072b7b9450bcd81bc
yuriykis <yurakis9@gmail.com>
2021-05-06 21:40:12 +0200

    add-login-form

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 import { Navbar } from './src/Navbar'
+import { Login } from './src/Login'
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Navbar title="Social App"></Navbar>
-      <View style={ styles.container }>
-      <StatusBar style="auto" />
+      <View style={styles.main}>
+        <Login style={styles.login_container}></Login>
+      </View>
     </View>
-    </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
   container: {
-    paddingHorizontal: 30,
-    paddingVertical: 20
+    flex: 1
   },
-});
+  main: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: '10%',
+    paddingVertical: '60%'
+  },
+  login_container: {
+    alignSelf: 'center'
+  }
+})
---
+++ src/Login.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { View, Text, TextInput, Button, Alert, StyleSheet } from 'react-native'
+
+export const Login = ( { onSubmit } ) => {
+    return (
+        <View style={styles.container}>
+        <TextInput
+        //   value={this.state.username}
+        //   onChangeText={(username) => this.setState({ username })}
+          placeholder={'Username'}
+          style={styles.input}
+        />
+        <TextInput
+        //   value={this.state.password}
+        //   onChangeText={(password) => this.setState({ password })}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        
+        <Button
+          title={'Login'}
+          style={styles.input}
+        //   onPress={this.onLogin.bind(this)}
+        />
+      </View>
+    )
+}
+
+const styles = StyleSheet.create( {
+    container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  }
+})
